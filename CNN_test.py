bd69ffbd48bc1537e21d9a6d0399194aac9fd54f
corcor27 <cory13579@googlemail.com>
2023-12-12 14:36:17 +0000

    Update CNN_test.py

--- CNN_test.py
+++ CNN_test.py
@@ -14,45 +14,8 @@ from tensorflow.keras.constraints import MaxNorm
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 import os
 
-forward_lstm = LSTM(units=320, return_sequences=True)
-# backward_lstm = LSTM(input_dim=320, output_dim=320, return_sequences=True)
-brnn = Bidirectional(forward_lstm)
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-print('building model')
-
-model = Sequential()
-model.add(Convolution1D(activation="relu", 
-                        input_shape=(1000, 4), 
-                        padding="valid", strides=1, 
-                        filters=320, kernel_size=26))
-
-model.add(MaxPooling1D(strides=13, pool_size=13))
-
-model.add(Dropout(0.2))
-
-model.add(brnn)
-
-model.add(Dropout(0.5))
-
-model.add(Flatten())
-
-model.add(Dense(input_dim=75*640, units=925))
-model.add(Activation('relu'))
-
-model.add(Dense(input_dim=925, units=919))
-model.add(Activation('sigmoid'))
-
-print('compiling model')
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['cosine'])
-
-print('running at most 60 epochs')
-
-checkpointer = ModelCheckpoint(filepath="DanQ_bestmodel.hdf5", verbose=1, save_best_only=True)
-earlystopper = EarlyStopping(monitor='val_loss', patience=5, verbose=1)
-
-model.summary()
-    
-          
     
                 
                 
